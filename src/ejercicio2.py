14289068176b021ffe0d7a50e948cd4def60ad36
Agustin Nahuelpan <agustin.d.nahuelpan@gmail.com>
2022-06-09 02:49:09 -0300

    Versión corregida donde cambio el ejercicio para que utilice sumas y restas

--- src/ejercicio2.py
+++ src/ejercicio2.py
@@ -11,13 +11,21 @@ Escribir una función que reciba un número e indique si el mismo es positivo, n
 
 def signo(numero):
     """Está función dice si el número ingresado es positivo, negativo o 0"""
-    resultado = 0 + numero #Cómo pedía que se utilizen sumas y restas, realiza esta cuenta, y dependiendo el resultado dice que tipo de numero es.
-    if resultado > 0:
-        regreso = "El número ingresado es positivo"
-    elif resultado < 0:
-        regreso = "El número ingresado es negativo"
-    else:
-        regreso = "El número ingresado es 0"
+    positivo = numero #Guarda numero dentro de una variable positivo para realizar operaciones.
+    negativo = numero #También guarda numero dentro de otra variable para realizar operaciones.
+    inicio = 1 #Una variable creada para mantener el while.
+    while  inicio > 0:
+        positivo = positivo + 1 #La variable con el mismo valor que el numero ingresado se le suma un 1. Si llega a 0 es porque el numero era positivo.
+        negativo = negativo - 1 #La variable con el mismo valor que el numero ingresado se le suma un 1. Si llega a 0 significa que era negativo.
+        if positivo == 0:
+            regreso = "El numero ingresado negativo"
+            inicio = 0
+        elif negativo == 0:
+            regreso = "El numero ingresado es positivo"
+            inicio = 0
+        elif negativo == -1 and positivo ==1: #La única forma que estos valores ocurran en simultaneo es si el numero ingresado es 0.
+            regreso = "El numero ingresado es 0"
+            inicio = 0
     return regreso
     
     pass
