9722255272a5eee773990a89ac47389cd77f9f20
Agustin Nahuelpan <agustin.d.nahuelpan@gmail.com>
2022-06-12 00:03:06 -0300

    El enunciado del problema no estaba como debería.

--- src/ejercicio1.py
+++ src/ejercicio1.py
@@ -2,14 +2,14 @@
 # Nombre - @agustin-nahuelpan
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
-
-#1. Conversión de temperaturas
+"""
+1. Conversión de temperaturas
 #Se quiere transformar temperaturas en grados fahrenheit a grados centígrados y viceversa.
-#Escribir las funciones para convertir la temperatura en grados centigrados en fahrenheit como un número decimal, utilice esta formula para calcular los grados centígrados y retorne el resultado obtenido. Y viceversa."""
+#Escribir las funciones para convertir la temperatura en grados centigrados en fahrenheit como un número decimal, utilice esta formula para calcular los grados centígrados y retorne el resultado obtenido. Y viceversa.
 
-#Precondiciones:Se debe ingresar un numero que expresa una temperatura en grados centigrados.
-#Poscondiciones:Se devolvera un número que representara una temperatura en grados fahrrenheit.
-
+Precondiciones:Se debe ingresar un numero que expresa una temperatura en grados centigrados.
+Poscondiciones:Se devolvera un número que representara una temperatura en grados fahrrenheit.
+"""
 def convertir_a_fahrrenheit(centigrados):
     salida = centigrados * 1.8 + 32
     """Esta función convierte grados centigrados a grados fahrrenheit."""
