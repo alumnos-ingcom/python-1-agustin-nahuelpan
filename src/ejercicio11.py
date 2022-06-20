b84dd1b6c764507a8ddce2bf13d9fbdf398d4a82
Agustin Nahuelpan <agustin.d.nahuelpan@gmail.com>
2022-06-20 04:05:59 -0300

    Agrega la función principal, agrega el docstring y elimina el pass

--- src/ejercicio11.py
+++ src/ejercicio11.py
@@ -5,13 +5,29 @@
 
 """
 11. Multiplos de
-Escribir una función que indique con True si un número entero es multiplo de otro, utilizando sumas y restas.
+Escribir una función que indique con True si un número entero es multiplo de otro, utilizando
+sumas y restas.
 """
-#Precondiciones: Deben ser dos numeros enteros.
-#Poscondiciones: Devolvera un valor lógico, o una cadena indicando que es falso.
 
+def principal():
+    """
+    Esta funcion se encarga de pedir los numetos y devolver si uno es multiplo del otro.
+    #Precondiciones: Deben ser dos numeros enteros.
+    Poscondiciones: Devolvera un valor lógico, o una cadena indicando que es falso.
+    """
+    print ("Ingrese un numero y después un segundo numero para verificar si este multiplo.")
+    numero = int(input("Ingrese el primer numero: "))
+    multiplo = int(input("Ingrese el segundo numero: "))
+    devolver = es_multiplo(numero, multiplo)
+    print (f"La prueba de verificación para averiguar si {multiplo} es multiplo de {numero} dio como resultado: {devolver}.")
+    
 
 def es_multiplo(numero, multiplo):
+    """
+    
+    Precondiciones: Deben ser dos numeros enteros.
+    Poscondiciones: Devolvera un valor lógico.
+    """
     cuenta = 1 #Necesaria para mantener el bucle while.
     devolver = False #A menos que se prueba que el segundo numero es multiplo del primero, la función devolvera que es falso.
     while cuenta == 1:
@@ -23,7 +39,6 @@ def es_multiplo(numero, multiplo):
             cuenta = 0
     return devolver
 
-    pass
-
 if __name__ == "__main__":
+    principal()
     es_multiplo(numero, multiplo)
