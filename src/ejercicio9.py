95fd316d001632cdf3d07201f42859597866c08c
Agustin Nahuelpan <agustin.d.nahuelpan@gmail.com>
2022-06-20 01:50:27 -0300

    Funcion principal agregada, docstring agregado, y pass eliminado.

--- src/ejercicio9.py
+++ src/ejercicio9.py
@@ -8,10 +8,22 @@
 Escribir una función que retorne una tuple con factores primos de un numero entero positivo.
 """
 
-#Precondiciones: Se debe ingresar un numero entero positivo.
-#Poscondiciones: Devolvera una tupla con los factores primos.
+def principal():
+    """
+    Esta función se encarga de la parte interactiva. Pide datos y devuelve los resultados.
+    Precondiciones: Se debe ingresar un numero entero positivo.
+    Poscondiciones: Devolvera una tupla con los factores primos.
+    """
+    numero = int(input("Ingrese un numero para identificar los numeros primos del mismo: "))
+    primos = factores_primos(numero)
+    print (F"Los factores primos de {numero} son {primos}(en caso de que salga 0 el numero introducido es primo.)")
 
 def factores_primos(numero):
+    """    
+    Esta función revisa cuales pueden ser numeros primos de un numero introducido.
+    Precondiciones: Se debe ingresar un numero entero positivo.
+    Poscondiciones: Devolvera una tupla con los factores primos.
+    """
     primos = [] #Se crea una tupla vacía para almacenar los factores primos.
     contador = numero
     aumento = 2 #Se establece el primer numero primo por el que va a empezar a dividir.
@@ -23,7 +35,6 @@ def factores_primos(numero):
     primos = tuple(primos)
     return primos
 
-    pass
-
 if __name__ == "__main__":
+    principal()
     factores_primos(numero)
